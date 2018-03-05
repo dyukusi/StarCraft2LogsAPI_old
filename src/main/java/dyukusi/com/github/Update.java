fc614b924e67e25fdb82c01ba49894b4f1f0cc65
Dyukusi <dyukusi.yukapero@gmail.com>
2018-03-06 02:40:31 +0900

    fix 500 glitch for Update

--- src/main/java/dyukusi/com/github/Update.java
+++ src/main/java/dyukusi/com/github/Update.java
@@ -30,30 +30,35 @@ public class Update {
             @FormParam("regionId")  int regionId,
             @FormParam("profileId") int profileId,
             @FormParam("raceId")    int raceId
-    ) throws SQLException, IOException {
-        Connection con = this.db.connect();
+    ) {
 
-        StringBuilder sql = new StringBuilder();
-        sql.append("SELECT * FROM profile_log WHERE region_id = ? AND id = ? AND race_id = ? ORDER BY created_at DESC;");
-        PreparedStatement ps = con.prepareStatement(sql.toString());
+        try {
+            Connection con = this.db.connect();
 
-        ps.setInt(1, regionId);
-        ps.setInt(2, profileId);
-        ps.setInt(3, raceId);
+            StringBuilder sql = new StringBuilder();
+            sql.append("SELECT * FROM profile_log WHERE region_id = ? AND id = ? AND race_id = ? ORDER BY created_at DESC;");
+            PreparedStatement ps = null;
 
-        System.out.println(ps.toString());
+            ps = con.prepareStatement(sql.toString());
+            ps.setInt(1, regionId);
+            ps.setInt(2, profileId);
+            ps.setInt(3, raceId);
 
-        ResultSet result = ps.executeQuery();
-        con.close();
+            ResultSet result = ps.executeQuery();
+            if (!result.next()) {
+                return "profile not found";
+            }
 
-        if (!result.next()) {
-            return "profile not found";
+            int ladderId = result.getInt("ladder_id");
+            con.close();
+
+            // update
+            API.updateProfile(regionId, ladderId, profileId);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         }
-
-        int ladderId = result.getInt("ladder_id");
-
-        // update
-        API.updateProfile(regionId, ladderId, profileId);
 
         return String.format("update process finished. regionId: %d, profileId: %d, raceId %d",
                 regionId,
