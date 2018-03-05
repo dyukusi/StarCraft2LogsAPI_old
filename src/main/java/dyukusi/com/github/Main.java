3ebaceb8f0068ac28d8727806904cf5ed285a8f1
Dyukusi <dyukusi.yukapero@gmail.com>
2018-03-05 23:08:26 +0900

    refactoring hostname

--- src/main/java/dyukusi/com/github/Main.java
+++ src/main/java/dyukusi/com/github/Main.java
@@ -1,6 +1,5 @@
 package dyukusi.com.github;
 
-import github.dyukusi.API;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
@@ -10,7 +9,7 @@ import java.net.URI;
 
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/";
+    public static final String BASE_URI = "http://0.0.0.0:8080/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
